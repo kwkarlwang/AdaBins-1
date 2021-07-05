4b601e0430b831614af03f3407f29e4928743225
Karl Wang <kawang@ucsd.edu>
2021-07-05 11:28:27 -0700

    Make copy

--- dataloader.py
+++ dataloader.py
@@ -353,7 +353,7 @@ class ToTensor(object):
                 'pic should be PIL Image or ndarray. Got {}'.format(type(pic)))
 
         if isinstance(pic, np.ndarray):
-            img = torch.from_numpy(pic.transpose((2, 0, 1)))
+            img = torch.from_numpy(np.array(pic.transpose((2, 0, 1))))
             return img
 
         # handle PIL Image
