11e451e86753e434175a3fd066936977f4acf7ca
Karl Wang <kawang@ucsd.edu>
2021-07-06 16:02:58 -0700

    Fix interpolation

--- train.py
+++ train.py
@@ -439,7 +439,7 @@ def validate(
             bins, pred, seg_out = model(img)
 
             seg_out = nn.functional.interpolate(
-                seg_out, seg.shape[-2:], mode="bilinear", align_corners=True
+                seg_out, seg.shape[-2:], mode="nearest", align_corners=True
             )
             seg_loss = seg_criterion(seg_out, seg)
             val_ce.append(seg_loss)
